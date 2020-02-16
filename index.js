d3479f22011e1c8b5238b4455e8dd3ac3ddb51bd
Jin Wu <jinn.wuu@gmail.com>
2020-02-16 22:02:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return driver;
 }
 
-function deleteFromDriverByKey(driver, key) {
+function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver[key];
+  return driver;
 }
